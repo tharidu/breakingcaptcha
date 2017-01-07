6edee744ca0981eaafb99784261516b97321ec13
Tharidu Fernando <tharidufernando@gmail.com>
2017-01-07 10:53:14 +0100

    Save images as jpg

--- data_gen/src/kth/id2223/Main.java
+++ data_gen/src/kth/id2223/Main.java
@@ -4,6 +4,7 @@ import nl.captcha.Captcha;
 import nl.captcha.backgrounds.FlatColorBackgroundProducer;
 import nl.captcha.gimpy.*;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.DataBufferByte;
@@ -24,14 +25,14 @@ public class Main extends JPanel {
     }
 
     public static void main(String[] args) throws IOException {
-        JFrame frame = new JFrame();
-        frame.getContentPane().add(new Main());
+//        JFrame frame = new JFrame();
+//        frame.getContentPane().add(new Main());
+//
+//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        frame.setSize(300, 300);
+//        frame.setVisible(true);
 
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(300, 300);
-        frame.setVisible(true);
-
-//        generateDatasets(1000);
+        generateDatasets(100);
     }
 
     public static void generateDatasets(long datasetSize) throws IOException {
@@ -46,6 +47,9 @@ public class Main extends JPanel {
 
             byte[] b = captcha.getAnswer().getBytes(StandardCharsets.US_ASCII);
             byte[] pixels = ((DataBufferByte) captcha.getImage().getRaster().getDataBuffer()).getData();
+
+            File outputfile = new File("imgs/" + captcha.getAnswer() + ".jpg");
+            ImageIO.write(captcha.getImage(), "jpg", outputfile);
 
             f.write(b);
             f.write(pixels);
